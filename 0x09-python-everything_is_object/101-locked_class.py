e7631a0b7238fc926c967e08cdd66b0e91e33c7e
Maxwell <anoliefomaxwell2@gmail.com>
2023-04-04 15:59:19 -0700

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -4,8 +4,8 @@
 
 class LockedClass:
     """
-    only allow instance with attributes 'first_name
+    only allow instance with
+    attributes 'first_name
     """
-
 
     __slots__ = ["first_name"]
