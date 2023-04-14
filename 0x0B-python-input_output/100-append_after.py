39450392de5f6f1c0811b78a836379901052e033
Maxwell <anoliefomaxwell2@gmail.com>
2023-04-13 17:31:13 -0700

    100-append_after.py

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" 13. Search and update """
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """ a function that inserts a line of text to a file,
+    after each line containing a specific string """
+    
+    res = ""
+    with open(filename) as r:
+        for line in r:
+            res += line
+            if search_string in line:
+                text += new_string
+    with open(filename, "w") as w:
+        w.write(res)
