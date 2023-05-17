1cda6bd1d240e84d25b070df4b9c69af448a5ffb
DevMaxwell <89994817+DevMaxwell@users.noreply.github.com>
2023-05-17 21:40:27 +0000

    12-no_genre.sql

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+-- left join tv_shows with tv_show_genres where genre_id is null
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
